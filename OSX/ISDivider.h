b0f2b7daf23386fdb00699a4225f109f056a69d4
InScopeApps <git@inscopeapps.com>
2011-03-27 15:24:01 -0700

    initial commit

---
+++ OSX/ISDivider.h
@@ -0,0 +1,17 @@
+//
+//  ISDivider.h
+//  InScopeLib
+//
+//  Created by David Keegan on 8/5/09.
+//  Copyright 2009 InScopeApps{+}. All rights reserved.
+//
+//  This is a subclass of NSSplitView that sets the line color to darkGrayColor.
+//
+
+@interface ISDivider : NSSplitView {}
+
+@end
+
+@interface ISDividerDelegate : NSObject <NSSplitViewDelegate> {}
+
+@end
