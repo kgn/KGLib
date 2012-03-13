94370375a8bc0259ebb31a8991daab243f6e19e2
David Keegan <git@davidkeegan.com>
2012-03-12 23:32:15 -0700

    Fixed a bug in the compiler conditional code
    This was caused by the conversion from a category on NSURL to NSString

--- Shared/NSString+KGFile.m
+++ Shared/NSString+KGFile.m
@@ -41,4 +41,56 @@
     return [newString substringFromIndex:i];
 }
 
+<<<<<<< Updated upstream
+=======
+- (NSURL *)volumeMountURLWithError:(NSError **)error{
+    NSURL *testURL = [NSURL fileURLWithPath:self];
+    NSMutableArray *pathComponents = [NSMutableArray array];
+    while(testURL != nil){
+        NSNumber *isVolume;
+        if(![testURL getResourceValue:&isVolume forKey:NSURLIsVolumeKey error:error]){
+            return nil;
+        }
+        
+        if([isVolume boolValue]){
+            break;
+        }
+        
+        [pathComponents insertObject:[testURL lastPathComponent] atIndex:0];
+        
+        NSURL *parentURL = nil;        
+        if(![testURL getResourceValue:&parentURL forKey:NSURLParentDirectoryURLKey error:error]){
+            return nil;
+        }
+        
+        testURL = parentURL;
+    }
+    
+    NSURL *volURL = nil;
+#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 1070
+    if(![[NSURL fileURLWithPath:self] getResourceValue:&volURL forKey:NSURLVolumeURLForRemountingKey error:error]){
+        return nil;
+    }
+#else
+    FSRef pathRef;
+    FSPathMakeRef((UInt8*)[self fileSystemRepresentation], &pathRef, NULL);
+    FSCatalogInfo catalogInfo;
+    OSErr osErr = FSGetCatalogInfo(&pathRef, kFSCatInfoVolume, &catalogInfo, NULL, NULL, NULL);
+    FSVolumeRefNum volumeRefNum = 0;
+    if(osErr == noErr){
+        volumeRefNum = catalogInfo.volume;
+    }
+    CFURLRef serverLocation;
+    OSStatus result = FSCopyURLForVolume(volumeRefNum, &serverLocation);
+    if(result == noErr){
+        volURL = (NSURL *)serverLocation;
+    }else{
+        return nil;
+    }   
+#endif
+    
+    return [volURL URLByAppendingPathComponent:[NSString pathWithComponents:pathComponents]];
+}
+
+>>>>>>> Stashed changes
 @end
